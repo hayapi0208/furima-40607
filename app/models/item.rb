a98d4ecaadfc56f965c8228dfc22a71964a46b90
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-09 15:26:11 +0900

    itemモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_one_attaches :image
+
+  validates :item, presence: true
+  validates :item_comment, presence: true
+  validates :item_category_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :delivery_tame_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :image, presence: true
 end

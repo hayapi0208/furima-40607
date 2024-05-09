488075a071487ae73435a023280bbb21cb3daddd
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-09 19:57:38 +0900

    商品出品機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item, :item_comment, :item_category_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :delivery_tame_id, :price, :image).merge(user_id: current_user.id)
+  end
 end

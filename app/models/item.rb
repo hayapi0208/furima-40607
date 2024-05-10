8e29d1cee3c5ed87fccf970de711439da22b3053
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-10 14:26:25 +0900

    Rubocopを実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_tame_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :image, presence: true
 end

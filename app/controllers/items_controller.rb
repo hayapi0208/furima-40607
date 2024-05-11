00f385f8f816cf6b5ed27a2ee47b81c00b619774
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-11 17:23:33 +0900

    本人以外は編集画面に遷移不可を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    return if @item.user == current_user
+
+    redirect_to action: :index
   end
 
   def update

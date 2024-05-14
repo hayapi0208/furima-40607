9c6ff152b68a1996b2b2b0553e7347ea92b4eeef
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-14 21:16:45 +0900

    クレカ情報のトークン化の処理を実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,23 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_***********************')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+      }
+    });
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);

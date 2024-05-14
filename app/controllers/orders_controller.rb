0527145f37daf9cee200efc8f44fefe9b14642d8
hayapi0208 <yhayasaska0208@gmail.com>
2024-05-14 22:18:51 +0900

    環境変数の設定、tokenにバリデーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
@@ -8,15 +9,11 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params.merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token]))
     if @order_address.valid?
-      Payjp.api_key = ""
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -25,5 +22,14 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,6 +9,7 @@ class OrderAddress
     validates :telephone, numericality: {only_integer: true}, length: {in: 10..11}, format: {with: /\A\d+\z/, message: "is invalid"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
